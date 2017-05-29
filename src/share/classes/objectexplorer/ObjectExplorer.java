64011817511f3c006c04f046f5477993da58f39a
zhangmingbo <mingbo.zhang@maveriqtech.com>
2017-05-29 17:33:51 +0800

    使用BTraceUtils中的sizeof方法代替MemoryMeasurer中的instrumentation.getObjectsize

--- src/share/classes/objectexplorer/ObjectExplorer.java
+++ src/share/classes/objectexplorer/ObjectExplorer.java
@@ -22,181 +22,187 @@ import java.util.*;
  * also return a value at the end of the traversal.
  */
 public class ObjectExplorer {
-  private ObjectExplorer() { }
+    static final Predicate<Chain> notEnumFieldsOrClasses = new Predicate<Chain>() {
 
-  /**
-   * Explores an object graph (defined by a root object and whatever is
-   * reachable through it, following non-static fields) while using an
-   * {@link ObjectVisitor} to both control the traversal and return a value.
-   *
-   * <p>Equivalent to {@code exploreObject(rootObject, visitor,
-   * EnumSet.noneOf(Feature.class))}.
-   *
-   * @param <T> the type of the value obtained (after the traversal) by the
-   * ObjectVisitor
-   * @param rootObject an object to be recursively explored
-   * @param visitor a visitor that is notified for each explored path and
-   * decides whether to continue exploration of that path, and constructs a
-   * return value at the end of the exploration
-   * @return whatever value is returned by the visitor at the end of the
-   * traversal
-   * @see ObjectVisitor
-   */
-  public static <T> T exploreObject(Object rootObject, ObjectVisitor<T> visitor) {
-    return exploreObject(rootObject, visitor, EnumSet.noneOf(Feature.class));
-  }
-
-  /**
-   * Explores an object graph (defined by a root object and whatever is
-   * reachable through it, following non-static fields) while using an
-   * {@link ObjectVisitor} to both control the traversal and return a value.
-   *
-   * <p>The {@code features} further customizes the exploration behavior.
-   * In particular:
-   * <ul>
-   * <li>If {@link Feature#VISIT_PRIMITIVES} is contained in features,
-   * the visitor will also be notified about exploration of primitive values.
-   * <li>If {@link Feature#VISIT_NULL} is contained in features, the visitor
-   * will also be notified about exploration of {@code null} values.
-   * </ul>
-   * In both cases above, the return value of
-   * {@link ObjectVisitor#visit(Chain)} is ignored, since neither primitive
-   * values or {@code null} can be further explored.
-   *
-   * @param <T> the type of the value obtained (after the traversal) by the
-   * ObjectVisitor
-   * @param rootObject an object to be recursively explored
-   * @param visitor a visitor that is notified for each explored path
-   * and decides whether to continue exploration of that path, and constructs
-   * a return value at the end of the exploration
-   * @param features a set of desired features that the object exploration should have
-   * @return whatever value is returned by the visitor at the end of the traversal
-   * @see ObjectVisitor
-   */
-  public static <T> T exploreObject(Object rootObject,
-      ObjectVisitor<T> visitor, EnumSet<Feature> features) {
-    Deque<Chain> stack = new ArrayDeque<Chain>(32);
-    if (rootObject != null) stack.push(Chain.root(rootObject));
-
-    while (!stack.isEmpty()) {
-      Chain chain = stack.pop();
-      //the only place where the return value of visit() is considered
-      Traversal traversal = visitor.visit(chain);
-      switch (traversal) {
-        case SKIP: continue;
-        case EXPLORE: break;
-      }
-
-      //only nonnull values pushed in the stack
-      @Nonnull Object value = chain.getValue();
-      Class<?> valueClass = value.getClass();
-      if (valueClass.isArray()) {
-        boolean isPrimitive = valueClass.getComponentType().isPrimitive();
-        for (int i = Array.getLength(value) - 1; i >= 0; i--) {
-          Object childValue = Array.get(value, i);
-          if (isPrimitive) {
-            if (features.contains(Feature.VISIT_PRIMITIVES))
-              visitor.visit(chain.appendArrayIndex(i, childValue));
-            continue;
-          }
-          if (childValue == null) {
-            if (features.contains(Feature.VISIT_NULL))
-              visitor.visit(chain.appendArrayIndex(i, childValue));
-            continue;
-          }
-          stack.push(chain.appendArrayIndex(i, childValue));
+        @Override
+        public boolean apply(Chain chain) {
+            return !(Enum.class.isAssignableFrom(chain.getValueType())
+                    || chain.getValue() instanceof Class<?>);
         }
-      } else {
-        for (Field field : getAllFields(value)) {
-          if (Modifier.isStatic(field.getModifiers())) continue;
-          Object childValue = null;
-          try {
-            childValue = field.get(value);
-          } catch (Exception e) {
-            throw new AssertionError(e);
-          }
-          if (childValue == null) {
-            if (features.contains(Feature.VISIT_NULL))
-              visitor.visit(chain.appendField(field, childValue));
-            continue;
-          }
-          boolean isPrimitive = field.getType().isPrimitive();
-          Chain extendedChain = chain.appendField(field, childValue);
-          if (isPrimitive) {
-            if (features.contains(Feature.VISIT_PRIMITIVES))
-              visitor.visit(extendedChain);
-            continue;
-          } else {
-            stack.push(extendedChain);
-          }
+    };
+    static final Predicate<Chain> skipTransientFields = new Predicate<Chain>() {
+
+        @Override
+        public boolean apply(Chain chain) {
+            if (chain instanceof FieldChain && (((FieldChain) chain).getField().getModifiers() & Modifier.TRANSIENT) != 0) {
+                return false;
+            } else {
+                return true;
+            }
         }
-      }
+    };
+    static final Function<Chain, Object> chainToObject =
+            new Function<Chain, Object>() {
+                public Object apply(Chain chain) {
+                    return chain.getValue();
+                }
+            };
+
+    private ObjectExplorer() {
     }
-    return visitor.result();
-  }
-
-  public static class AtMostOncePredicate implements Predicate<Chain> {
-    private final Set<Object> interner = Collections.newSetFromMap(
-        new IdentityHashMap<Object, Boolean>());
-
-    public boolean apply(Chain chain) {
-      Object o = chain.getValue();
-      return o instanceof Class<?> || interner.add(o);
-    }
-  }
-
-  static final Predicate<Chain> notEnumFieldsOrClasses = new Predicate<Chain>(){
-    public boolean apply(Chain chain) {
-      return !(Enum.class.isAssignableFrom(chain.getValueType())
-          || chain.getValue() instanceof Class<?>);
-    }
-  };
-  
-  static final Predicate<Chain> skipTransientFields = new Predicate<Chain>(){
-	    public boolean apply(Chain chain) {
-			if (chain instanceof FieldChain && (((FieldChain) chain).getField().getModifiers() & Modifier.TRANSIENT) != 0) {
-				return false;
-			} else {
-				return true;
-			}
-	    }
-  };  
-
-  static final Function<Chain, Object> chainToObject =
-    new Function<Chain, Object>() {
-    public Object apply(Chain chain) {
-      return chain.getValue();
-    }
-  };
-
-  private static Iterable<Field> getAllFields(Object o) {
-    List<Field> fields = Lists.newArrayListWithCapacity(8);
-    Class<?> clazz = o.getClass();
-    while (clazz != null) {
-      fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
-      clazz = clazz.getSuperclass();
-    }
-
-    //all together so there is only one security check
-    AccessibleObject.setAccessible(fields.toArray(new AccessibleObject[fields.size()]), true);
-    return fields;
-  }
-
-  /**
-   * Enumeration of features that may be optionally requested for an object
-   * traversal.
-   *
-   * @see ObjectExplorer#exploreObject(Object, ObjectVisitor, EnumSet)
-   */
-  public enum Feature {
-    /**
-     * Null references should be visited.
-     */
-    VISIT_NULL,
 
     /**
-     * Primitive values should be visited.
+     * Explores an object graph (defined by a root object and whatever is
+     * reachable through it, following non-static fields) while using an
+     * {@link ObjectVisitor} to both control the traversal and return a value.
+     * <p>
+     * <p>Equivalent to {@code exploreObject(rootObject, visitor,
+     * EnumSet.noneOf(Feature.class))}.
+     *
+     * @param <T>        the type of the value obtained (after the traversal) by the
+     *                   ObjectVisitor
+     * @param rootObject an object to be recursively explored
+     * @param visitor    a visitor that is notified for each explored path and
+     *                   decides whether to continue exploration of that path, and constructs a
+     *                   return value at the end of the exploration
+     * @return whatever value is returned by the visitor at the end of the
+     * traversal
+     * @see ObjectVisitor
      */
-    VISIT_PRIMITIVES
-  }
+    public static <T> T exploreObject(Object rootObject, ObjectVisitor<T> visitor) {
+        return exploreObject(rootObject, visitor, EnumSet.noneOf(Feature.class));
+    }
+
+    /**
+     * Explores an object graph (defined by a root object and whatever is
+     * reachable through it, following non-static fields) while using an
+     * {@link ObjectVisitor} to both control the traversal and return a value.
+     * <p>
+     * <p>The {@code features} further customizes the exploration behavior.
+     * In particular:
+     * <ul>
+     * <li>If {@link Feature#VISIT_PRIMITIVES} is contained in features,
+     * the visitor will also be notified about exploration of primitive values.
+     * <li>If {@link Feature#VISIT_NULL} is contained in features, the visitor
+     * will also be notified about exploration of {@code null} values.
+     * </ul>
+     * In both cases above, the return value of
+     * {@link ObjectVisitor#visit(Chain)} is ignored, since neither primitive
+     * values or {@code null} can be further explored.
+     *
+     * @param <T>        the type of the value obtained (after the traversal) by the
+     *                   ObjectVisitor
+     * @param rootObject an object to be recursively explored
+     * @param visitor    a visitor that is notified for each explored path
+     *                   and decides whether to continue exploration of that path, and constructs
+     *                   a return value at the end of the exploration
+     * @param features   a set of desired features that the object exploration should have
+     * @return whatever value is returned by the visitor at the end of the traversal
+     * @see ObjectVisitor
+     */
+    public static <T> T exploreObject(Object rootObject,
+                                      ObjectVisitor<T> visitor, EnumSet<Feature> features) {
+        Deque<Chain> stack = new ArrayDeque<Chain>(32);
+        if (rootObject != null) stack.push(Chain.root(rootObject));
+
+        while (!stack.isEmpty()) {
+            Chain chain = stack.pop();
+            //the only place where the return value of visit() is considered
+            Traversal traversal = visitor.visit(chain);
+            switch (traversal) {
+                case SKIP:
+                    continue;
+                case EXPLORE:
+                    break;
+            }
+
+            //only nonnull values pushed in the stack
+            @Nonnull Object value = chain.getValue();
+            Class<?> valueClass = value.getClass();
+            if (valueClass.isArray()) {
+                boolean isPrimitive = valueClass.getComponentType().isPrimitive();
+                for (int i = Array.getLength(value) - 1; i >= 0; i--) {
+                    Object childValue = Array.get(value, i);
+                    if (isPrimitive) {
+                        if (features.contains(Feature.VISIT_PRIMITIVES))
+                            visitor.visit(chain.appendArrayIndex(i, childValue));
+                        continue;
+                    }
+                    if (childValue == null) {
+                        if (features.contains(Feature.VISIT_NULL))
+                            visitor.visit(chain.appendArrayIndex(i, childValue));
+                        continue;
+                    }
+                    stack.push(chain.appendArrayIndex(i, childValue));
+                }
+            } else {
+                for (Field field : getAllFields(value)) {
+                    if (Modifier.isStatic(field.getModifiers())) continue;
+                    Object childValue = null;
+                    try {
+                        childValue = field.get(value);
+                    } catch (Exception e) {
+                        throw new AssertionError(e);
+                    }
+                    if (childValue == null) {
+                        if (features.contains(Feature.VISIT_NULL))
+                            visitor.visit(chain.appendField(field, childValue));
+                        continue;
+                    }
+                    boolean isPrimitive = field.getType().isPrimitive();
+                    Chain extendedChain = chain.appendField(field, childValue);
+                    if (isPrimitive) {
+                        if (features.contains(Feature.VISIT_PRIMITIVES))
+                            visitor.visit(extendedChain);
+                        continue;
+                    } else {
+                        stack.push(extendedChain);
+                    }
+                }
+            }
+        }
+        return visitor.result();
+    }
+
+    private static Iterable<Field> getAllFields(Object o) {
+        List<Field> fields = Lists.newArrayListWithCapacity(8);
+        Class<?> clazz = o.getClass();
+        while (clazz != null) {
+            fields.addAll(Arrays.asList(clazz.getDeclaredFields()));
+            clazz = clazz.getSuperclass();
+        }
+
+        //all together so there is only one security check
+        AccessibleObject.setAccessible(fields.toArray(new AccessibleObject[fields.size()]), true);
+        return fields;
+    }
+
+    /**
+     * Enumeration of features that may be optionally requested for an object
+     * traversal.
+     *
+     * @see ObjectExplorer#exploreObject(Object, ObjectVisitor, EnumSet)
+     */
+    public enum Feature {
+        /**
+         * Null references should be visited.
+         */
+        VISIT_NULL,
+
+        /**
+         * Primitive values should be visited.
+         */
+        VISIT_PRIMITIVES
+    }
+
+    public static class AtMostOncePredicate implements Predicate<Chain> {
+        private final Set<Object> interner = Collections.newSetFromMap(
+                new IdentityHashMap<Object, Boolean>());
+
+        @Override
+        public boolean apply(Chain chain) {
+            Object o = chain.getValue();
+            return o instanceof Class<?> || interner.add(o);
+        }
+    }
 }
